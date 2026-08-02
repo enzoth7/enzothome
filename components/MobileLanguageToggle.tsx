cad978459f238bbef719b216e945280b412a1bf5
gaston10com <enzothome1@gmail.com>
2026-08-02 00:02:48 -0300

    chore

--- components/MobileLanguageToggle.tsx
+++ components/MobileLanguageToggle.tsx
@@ -4,7 +4,7 @@ import LanguageToggle from "@/src/components/LanguageToggle";
 
 export default function MobileLanguageToggle() {
   return (
-    <div className="fixed bottom-8 right-8 z-[150] block md:hidden">
+    <div className="fixed bottom-8 left-8 z-[150] block md:hidden">
       <LanguageToggle inline dark />
     </div>
   );
